e0f032b2558b2479aaceaeab07e8bb09133ec58c
NagarjunaKadari1008 <nagarjuna@veefin.com>
2024-01-22 21:16:47 +0530

    1

--- Hello.py
+++ Hello.py
@@ -1,7 +1,18 @@
 print("Hello")
 
 def hello():
+    hbckjd
+    nckdckmdkc
+    ncdkncdkckd
     var1=50
+    cbdjncdnknc
     var2=30
-    diff=var1-var2
-    print("The difference between",var1,"and",var2,"is:",diff)
+    bcdijcikj
+    print()
+
+     msclsclls
+     mcs,cccjeieincecndiff=var1-var2
+     msclscljeieincecnls
+     mcs,cccjeieincecnprint("The difference between",var1,"and",var2,"is:",diff)
+ msclscllsjeieincecn
+ mcs,cccjeieincecn
